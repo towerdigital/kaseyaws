e3cec5e00816c9bc49e0ed6b9ebf0b979fe25012
Phillip Henslee <ph2@ph2.us>
2013-04-24 17:53:19 -0500

    Fixed bug in method get_all_alarms

--- lib/kaseyaws.rb
+++ lib/kaseyaws.rb
@@ -9,15 +9,18 @@ module KaseyaWS
 
     def initialize (username,password,hostname)
 
+      # The FQDN of the Kaseya web service
       @serviceurl = "https://" + hostname + "/vsaWS/kaseyaWS.asmx?WSDL"
-
+      # Get the clients IP address
+      @client_ip = KaseyaWS::Security.client_ip
+      # Hash savon option for reuse
       @savon_options={
         wsdl: @serviceurl,
         convert_request_keys_to: :camelcase,
         env_namespace: :soap,
         open_timeout: 30
       }
-
+      # Autheticate with web service and get session id
       @sessionid = self.authenticate(username,password)
 
     end
@@ -25,8 +28,8 @@ module KaseyaWS
     def authenticate(username,password)
 
       random_number = KaseyaWS::Security.secure_random(8)
-      covered_password = Kaseya::Security.compute_covered_password(username,password, random_number, HASH_ALGORITHM)
-      browser_ip = Kaseya::Security.client_ip
+      covered_password = KaseyaWS::Security.compute_covered_password(username,password, random_number, HASH_ALGORITHM)
+      browser_ip = @client_ip
 
       client = Savon.client(@savon_options)
 
@@ -38,7 +41,6 @@ module KaseyaWS
                              hashing_algorithm: HASH_ALGORITHM}]}
                              )
       @sessionid = response.body[:authenticate_response][:authenticate_result][:session_id]
-      response
 
     end
 
@@ -48,7 +50,7 @@ module KaseyaWS
 
       response = client.call(:get_alarm_list, message: {req:[{
                                                                return_all_records: get_all_records,
-                                                               browser_ip: get_client_ip ,
+                                                               browser_ip: @client_ip,
                              session_i_d: @sessionid}]}
                              )
       response.hash[:envelope][:body][:get_alarm_list_response][:get_alarm_list_result]
