bc12bb530f25d6c3cd94980b5b5fddfc9ed7924c
Phillip Henslee <ph2@ph2.us>
2013-04-28 18:53:10 -0500

    Corrected typos and structure of comments

--- lib/kaseyaws/security.rb
+++ lib/kaseyaws/security.rb
@@ -18,16 +18,14 @@ require 'json'
 module KaseyaWS
   class Security
 
-    # Utility method to return the client's IP address
-    # Most but not all Kaseya VSA SOAP request require <BrowserIP>
+    # Utility method to return the client's IP address, most but not all Kaseya VSA SOAP request require it.
 
     def self.client_ip
       r =  Net::HTTP.get( 'jsonip.com','/' )
       r = JSON::parse(r)['ip']
     end
 
-    # Create a secure random number, returns a eight digit string
-    # Used to compute the second hash for the double hash sequence
+    # Returns an eight digit secure random number, used to compute the second hash for the double hash sequence.
     
     def self.secure_random
       i = SecureRandom.random_bytes
@@ -36,7 +34,7 @@ module KaseyaWS
       i = i[1..8]
     end
 
-    # Computes the double hashed covered password to authenticate with the VSA
+    # Computes the double hashed covered password to authenticate with the VSA server.
     
     def self.compute_covered_password(username, password, rnd_number, hashing_algorithm)
       if hashing_algorithm == "SHA-1"
