d29692782673bcb8345a0707832f8da3ab5bca30
Phillip Henslee <ph2@ph2.us>
2013-04-28 14:24:23 -0500

    Added comments to all security methods

--- lib/kaseyaws/security.rb
+++ lib/kaseyaws/security.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright 2013 by Phillip Henslee (ph2@ph2.us).
+# Copyright 2013 by Phillip Henslee (phenslee@towerdigital.us).
 # All rights reserved.
 
 # Permission is granted for use, copying, modification, distribution,
@@ -18,11 +18,17 @@ require 'json'
 module KaseyaWS
   class Security
 
+    # Utility method to return the client's IP address
+    # Most but not all Kaseya VSA SOAP request require <BrowserIP>
+
     def self.client_ip
       r =  Net::HTTP.get( 'jsonip.com','/' )
       r = JSON::parse(r)['ip']
     end
 
+    # Create a secure random number, returns a eight digit string
+    # Used to compute the second hash for the double hash sequence
+    
     def self.secure_random
       i = SecureRandom.random_bytes
       i = i.each_byte.map { |b| b.to_s }.join
@@ -30,6 +36,8 @@ module KaseyaWS
       i = i[1..8]
     end
 
+    # Computes the double hashed covered password to authenticate with the VSA
+    
     def self.compute_covered_password(username, password, rnd_number, hashing_algorithm)
       if hashing_algorithm == "SHA-1"
         hash1 = Digest::SHA1.hexdigest(password + username)
