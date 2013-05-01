1f125c30ca7364a32957045243a836f922002b19
Phillip Henslee <ph2@ph2.us>
2013-05-01 18:41:46 -0500

    Bumped version to 0.0.6

--- lib/kaseyaws/version.rb
+++ lib/kaseyaws/version.rb
@@ -2,7 +2,7 @@ module KaseyaWS
   VERSION_NUMBERS = [
     VERSION_MAJOR = 0,
     VERSION_MINOR = 0,
-    VERSION_BUILD = 5,
+    VERSION_BUILD = 6,
   ]
   VERSION = VERSION_NUMBERS.join(".")
 end
